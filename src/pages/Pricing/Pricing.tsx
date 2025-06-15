05746f7428ed161335df7b94ecb25c677fbf1b6a
bpathi <bpathi@bpathi-M-IN.local>
2025-06-15 22:58:40 +0530

    Adding the admin-fe initial version. this has been stripped out from go-admin repo.

---
+++ src/pages/Pricing/Pricing.tsx
@@ -0,0 +1,9 @@
+import { SubScriptionTierTab } from './SubscriptionTier';
+
+export function PricingPage () {
+    return (
+        <div className = 'container'>
+            <SubScriptionTierTab />
+        </div>
+    )
+}
---
+++ src/pages/Usage/Usage.tsx
@@ -0,0 +1,7 @@
+export function UsagePage () {
+    return (
+        <div className = 'container'>
+            <h1>Usage will go here !!</h1>
+        </div>
+    )
+}
